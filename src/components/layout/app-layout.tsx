a5844760ce9f62ecb7990bb6ea33063f104ae07a
Hà Quang Nguyên <hqnguyenvn@gmail.com>
2026-01-18 09:44:15 +0000

    Vấn đề vẫn chưa được fix

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -9,13 +9,13 @@ import { SidebarNav } from "./sidebar-nav";
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex h-screen flex-1 flex-col overflow-hidden bg-background">
+      <div className="flex h-screen w-full flex-col overflow-hidden bg-background">
         <AppHeader />
         <div className="flex flex-1 overflow-hidden">
           <Sidebar>
             <SidebarNav />
           </Sidebar>
-          <SidebarInset className="overflow-y-auto">
+          <SidebarInset className="overflow-y-auto p-4 md:p-8 pt-6">
              {children}
           </SidebarInset>
         </div>
