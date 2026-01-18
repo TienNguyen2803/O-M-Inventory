e98a10171916db917e77eef965f38267f3ea4c6b
Hà Quang Nguyên <hqnguyenvn@gmail.com>
2026-01-18 09:37:56 +0000

    header vẫn không bung hết chiều ngang trình duyệt mà nó thay đổi kích th

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -9,13 +9,13 @@ import { SidebarNav } from "./sidebar-nav";
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex h-screen flex-col overflow-hidden bg-background">
+      <div className="flex h-screen flex-1 flex-col overflow-hidden bg-background">
         <AppHeader />
         <div className="flex flex-1 overflow-hidden">
           <Sidebar>
             <SidebarNav />
           </Sidebar>
-          <SidebarInset className="overflow-y-auto pt-6 px-4 md:px-8">
+          <SidebarInset className="overflow-y-auto">
              {children}
           </SidebarInset>
         </div>
